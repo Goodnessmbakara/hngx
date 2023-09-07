dca88098d8d0da187c4397e75e8accd3d8ac1932
Goodnessmbakara <amicablembakara50@gmail.com>
2023-09-07 13:45:21 +0100

    changed format

--- get_endpoints/urls.py
+++ get_endpoints/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.InformationView.as_view(), name="get_info_endpoint"),
+    path("api/", views.InformationView.as_view(), name="get_info_endpoint"),
 ]
