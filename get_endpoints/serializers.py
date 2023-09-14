3f396da7fdc28b02826a06da51a44aa016051895
Goodness Mbakara <amicablembakara50@gmail.com>
2023-09-15 00:18:47 +0100

    Update serializers.py

--- get_endpoints/serializers.py
+++ get_endpoints/serializers.py
@@ -2,9 +2,14 @@
 from rest_framework import serializers
 from .models import Person
 
+def validate_unique_name(value):
+    if Person.objects.filter(name=value).exists():
+        raise serializers.ValidationError("This name already exists. Please choose a different name.")
+
 class PersonSerializer(serializers.ModelSerializer):
-    """serializer foe the Person model"""
+    name = serializers.CharField(max_length=255, validators=[validate_unique_name])
+
     class Meta:
         model = Person
-        fields = ('id', 'name')
+        fields = '__all__'
 
